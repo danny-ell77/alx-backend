7347b3898c04b5e0fde0f39d398c417ddbe95717
Olah Daniel <danielolahskybrow77@gmail.com>
2023-08-01 10:21:38 +0100

    i18n tasks done

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
